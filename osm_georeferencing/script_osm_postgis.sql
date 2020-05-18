992a8fb04d4a6dba84f485bdba09e8e4b6d49f2d
Franck Theeten <franck.theeten@africamuseum.be>
2020-05-18 22:30:44 +0200

    Update script_osm_postgis.sql

--- osm_georeferencing/script_osm_postgis.sql
+++ osm_georeferencing/script_osm_postgis.sql
@@ -19,6 +19,48 @@ AJAX WRAPPER
 
 */
 
+-- Function: urlencode(text)
+
+-- DROP FUNCTION urlencode(text);
+
+CREATE OR REPLACE FUNCTION urlencode(IN in_str text, OUT _result text)
+  RETURNS text AS
+$BODY$
+DECLARE
+    _i      int4;
+    _temp   varchar;
+    _ascii  int4;
+BEGIN
+    _result = '';
+    FOR _i IN 1 .. length(in_str) LOOP
+        _temp := substr(in_str, _i, 1);
+        IF _temp ~ '[0-9a-zA-Z:/@._?#-]+' THEN
+            _result := _result || _temp;
+        ELSE
+            _ascii := ascii(_temp);
+            IF _ascii > x'07ff'::int4 THEN
+                RAISE EXCEPTION 'Won''t deal with 3 (or more) byte sequences. (%)', in_str;
+            END IF;
+            IF _ascii <= x'07f'::int4 THEN
+                _temp := '%'||to_hex(_ascii);
+            ELSE
+                _temp := '%'||to_hex((_ascii & x'03f'::int4)+x'80'::int4);
+                _ascii := _ascii >> 6;
+                _temp := '%'||to_hex((_ascii & x'01f'::int4)+x'c0'::int4)
+                            ||_temp;
+            END IF;
+            _result := _result || upper(_temp);
+        END IF;
+    END LOOP;
+    RETURN ;
+END;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE STRICT
+  COST 100;
+ALTER FUNCTION urlencode(text)
+  OWNER TO darwin2;
+
+
 --- Function: fct_rmca_py_webservice(text, text, text)
 
 -- DROP FUNCTION fct_rmca_py_webservice(text, text, text);
