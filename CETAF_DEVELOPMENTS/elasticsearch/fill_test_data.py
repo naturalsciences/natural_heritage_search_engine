c067da4153826296224623b296d1ec24ed804481
Franck Theeten <franck.theeten@africamuseum.be>
2020-07-23 01:07:54 +0200

    Update fill_test_data.py

--- CETAF_DEVELOPMENTS/elasticsearch/fill_test_data.py
+++ CETAF_DEVELOPMENTS/elasticsearch/fill_test_data.py
@@ -130,6 +130,122 @@ def create_coll(museum, coll, parent_coll=None):
     
     returned_2["manager_head_of_collection"]={"manager_title":"Dr.", "manager_name":"Syd Barrett", "manager_email":"syd.barrett@cetaf.be", "manager_research_fields":['Mycology','Anthropology']}
     returned_2["parent_relationship"]=relation
+    
+    collection_portal_fields1={}
+    collection_portal_fields1["collection_page"]="Global search portal"
+    collection_portal_fields1["collection_page_url"]="http://www.naturalheritage.be/"
+    
+    collection_portal_fields2={}
+    collection_portal_fields2["collection_page"]="collection portal"
+    collection_portal_fields2["collection_page_url"]="http://www.naturalsciences.be/"
+    returned_2["collection_portal_fields"]=[collection_portal_fields1, collection_portal_fields2]
+    
+    description_fields={}
+    description_fields["accession_specimens"]=lorem_ipsum("accession_specimens1", iCols )
+    description_fields["collection_statistics"]="<table name='results1' id='results1' class='results'><tbody><tr><th>collection name</th><th>nb database records</th><th>nb physical specimens low</th><th>nb physical specimens high</th></tr><tr><td>Alex Fain's Acari Collection</td><td>63045</td><td>81013</td><td>81251</td></tr><tr><td>Alexis Robert</td><td>32</td><td>89</td><td>89</td></tr><tr><td>Algemeen</td><td>899</td><td>4873</td><td>4873</td></tr><tr><td>Arsène Fouassin</td><td>3997</td><td>5686</td><td>5686</td></tr><tr><td>Belgian Ceratopogonidae Collection</td><td>884</td><td>1573</td><td>1573</td></tr><tr><td>Belgian Coleoptera Collection</td><td>5180</td><td>258057</td><td>258057</td></tr><tr><td>Belgian Heterocera Collection</td><td>3007</td><td>120784</td><td>120784</td></tr><tr><td>Belgian Mecoptera Collection</td><td>5</td><td>543</td><td>543</td></tr><tr><td>Belgian Megaloptera Collection</td><td>4</td><td>241</td><td>241</td></tr><tr><td>Belgian Neuroptera Collection</td><td>63</td><td>1456</td><td>1456</td></tr><tr><td>Belgian Orthoptera Collection</td><td>179</td><td>5300</td><td>5300</td></tr><tr><td>Belgian Rhopalocera Collection</td><td>1507</td><td>51332</td><td>51332</td></tr><tr><td>BE-RBINS Entomology</td><td>1</td><td>1</td><td>1</td></tr><tr><td>Chapelle J.</td><td>6</td><td>6</td><td>6</td></tr><tr><td>Collection M. Bequaert</td><td>130</td><td>190</td><td>190</td></tr><tr><td>Collection M. Goetghebuer</td><td>256</td><td>387</td><td>387</td></tr><tr><td>Collection of protected insects</td><td>8</td><td>8</td><td>8</td></tr><tr><td>Collection Voyage of H. R. H. the Prince Leopold (1929)</td><td>963</td><td>1614</td><td>1614</td></tr><tr><td>Collection Voyage of H. R. H. the Prince Leopold (1932)</td><td>703</td><td>1688</td><td>1688</td></tr><tr><td>Coll Fain /1</td><td>83</td><td>91</td><td>91</td></tr><tr><td>Derenne Emile</td><td>1195</td><td>4976</td><td>4976</td></tr><tr><td>Didactic collection</td><td>13</td><td>13</td><td>13</td></tr><tr><td>Diptera Belgium</td><td>6180</td><td>6241</td><td>6241</td></tr><tr><td>Diptera General</td><td>15230</td><td>20537</td><td>20537</td></tr><tr><td>Drumont Alain</td><td>47</td><td>73</td><td>73</td></tr><tr><td>External Xylobiontes Collection</td><td>1</td><td>1</td><td>1</td></tr><tr><td>Galant M.</td><td>23</td><td>38</td><td>38</td></tr><tr><td>General Araneae Collection</td><td>1275</td><td>1710</td><td>1710</td></tr><tr><td>General Coleoptera Collection</td><td>15570</td><td>386694</td><td>386694</td></tr><tr><td>General Collection</td><td>2797</td><td>2797</td><td>2797</td></tr><tr><td>General Heterocera Collection</td><td>307</td><td>608</td><td>608</td></tr><tr><td>General Orthoptera Collection</td><td>1712</td><td>38432</td><td>38432</td></tr><tr><td>General Rhopalocera Collection</td><td>12480</td><td>152617</td><td>152617</td></tr><tr><td>Guilleaume Félix</td><td>207</td><td>363</td><td>363</td></tr><tr><td>Hennuy J. J.</td><td>14</td><td>22</td><td>22</td></tr><tr><td>H. M. King Leopold III</td><td>658</td><td>949</td><td>949</td></tr><tr><td>H. M. Queen Elisabeth (Brazilian Lepidoptera)</td><td>1483</td><td>1556</td><td>1556</td></tr><tr><td>Jacobs</td><td>2</td><td>3</td><td>3</td></tr><tr><td>J.P. Smeekens</td><td>9</td><td>203</td><td>203</td></tr><tr><td>Leroux</td><td>458</td><td>1054</td><td>1054</td></tr><tr><td>Mal Noël</td><td>28</td><td>51</td><td>51</td></tr><tr><td>Maquet Daniel</td><td>19</td><td>68</td><td>68</td></tr><tr><td>M.-E. de Selys-Longchamps</td><td>2131</td><td>6792</td><td>6792</td></tr><tr><td>Miessen Geoffrey</td><td>73</td><td>114</td><td>114</td></tr><tr><td>Muret P.</td><td>8</td><td>12</td><td>12</td></tr><tr><td>RBINS Xylobiontes Collection</td><td>18279</td><td>208018</td><td>208018</td></tr><tr><td>Reference collection</td><td>54</td><td>54</td><td>54</td></tr><tr><td>Ronald Brabant</td><td>4085</td><td>4984</td><td>4984</td></tr><tr><td>Supplementen</td><td>714</td><td>2353</td><td>2353</td></tr><tr><td>Types Virtual Collections</td><td>3715</td><td>4164</td><td>4164</td></tr><tr><td>Van Dorsselaer</td><td>118</td><td>5002</td><td>5002</td></tr><tr><td><b>TOTAL</b></td><td><b>169837</b></td><td><b>1385431</b></td><td><b>1385669</b></td></tr></tbody></table>"   
+
+    description_fields["genetic_repository"]=["Barcode of life", "GenBank"]
+    description_fields["genetic_repository_link"]=["https://ibol.org/", "https://www.ncbi.nlm.nih.gov/genbank/"]
+    description_fields["other_size_indicators"]="Required physical storage space : 240 m3"
+    description_fields["outstanding_collection_features"]="Holotype of Afropavo (Congolese Peacock)"
+    returned_2["description_fields"]=description_fields
+    
+    edu_contact1={}
+    edu_contact1["edu_contact_type"]="Organizer of the Summer school"
+    edu_contact1["edu_email"]="test1@gmail.com"
+    edu_contact1["edu_name"]="Kim Jacobsen"
+    edu_contact1["edu_phone"]="+32 XXXXX"
+    edu_contact1["edu_title"]="Dr."
+    
+    edu_contact2={}
+    edu_contact2["edu_contact_type"]="Registration officer"
+    edu_contact2["edu_email"]="test2@gmail.com"
+    edu_contact2["edu_name"]="Ian Smith"
+    edu_contact2["edu_phone"]="+32 4XXXX"
+    
+    education={}
+    education["education_general_description"]="Summer school on the Electric Discharge organs of fishes"
+    education_detail={}
+    education_detail["training_online_or_presential"]="presential"
+    education_detail["training_type"]="summer school (No ECTS)"
+    education["training"]=education_detail
+    education["contact_person"]=[edu_contact1, edu_contact2]
+    returned_2["education"]=education
+    
+    facility1={}
+    facility1["available_tools"]=["PCR", "Restriction ensymes for DNA barcode", "4 core machines", "TASSEL GBS software"]
+    facility1["facility_description"]="JEMU genetic laboratorys"
+    facility1["list_of_facilities"]=[ "Fozen storage room", "Computer room"]
+    facility1["list_of_laboratories"]=["PCR room", "Molecular analysis room"]
+    
+    facility2={}
+    facility2["available_tools"]=["Miscroscope", "X-Ray machine"]
+    facility2["facility_description"]="Ichtyological room"
+    facility2["list_of_facilities"]=[ "Storage room", "Training room"]
+    facility2["list_of_laboratories"]=["Meristic unit room"]
+    returned_2["facility"]=[facility1, facility2]
+    
+    mngr_contact={}
+    mngr_contact["manager_position"]="Head of collection"
+    mngr_contact["manager_email"]="test1@gmail.com"
+    mngr_contact["manager_name"]="Ernst Lubitsch"
+    mngr_contact["manager_phone"]="+32 XXXXX"
+    mngr_contact["manager_title"]="Dr."
+    mngr_contact["manager_research_fields"]=["Anthropology","Political sciences", "Esthetics", "Sociology" ]
+    returned_2["manager_head_of_collection"]=mngr_contact
+    
+    natural_history_collection={}
+    natural_history_collection["abstract"]=lorem_ipsum("Abstract of collection",iCols)
+    natural_history_collection["acquisition_source"]=["Donation", "Field work", "Merging of institution collections"]
+    natural_history_collection["codes"]=["COL1-NHM", "10.1000/xyz123"]
+    contact1={}
+    contact1["adm_contact_type"]="Secretary of Vertrebrate Department"
+    contact1["adm_email"]="test1@gmail.com"
+    contact1["adm_name"]="Paul Canon"
+    contact1["adm_phone"]="+32 XXXXX"
+    administration_field={}
+    administration_field["contact_person"]=contact1
+    administration_field["legal_property"]="Collection owned by XXX"
+    administration_field["status"]="Free access for academic use"
+    natural_history_collection["administration_field"]=administration_field
+    
+
+    staff1={}
+    staff1["staff_collection_role"]="Researcher in entomology"
+    staff1["staff_curatorial_role"]="Scientific curator"
+    staff1["staff_degree_msc_discipline"]="Agrononmy"
+    staff1["staff_degree_msc_title"]="Economic impact of the introduction of lobster in technical mangroves"
+    staff1["staff_description"]=lorem_ipsum_short("staff 1", iCols)
+    staff1["staff_expertise_description"]="Agronomy and taxonomi"
+    staff1["staff_expertise_domain"]=["Ecology of mangrove", "ecology of small lakes"]
+    staff1["staff_expertise_geography"]="Adriatic sea"
+    staff1["staff_name"]="Jacqueline Robson"
+    staff1["staff_full_time_equivalent_pc"]=90
+    si1={}
+    si1["value"]="0000-0002-0626-538X"
+    si1["system"]="ORCID"
+    staff1["staff_identifiers"]=si1
+    
+    staff2={}
+    staff2["staff_collection_role"]="Researcher in entomology"
+    staff2["staff_curatorial_role"]="Scientific curator"
+    staff2["staff_degree_msc_discipline"]="Agrononmy"
+    staff2["staff_degree_msc_title"]="Economic impact of the introduction of lobster in technical mangroves"
+    staff2["staff_description"]=lorem_ipsum_short("staff 1", iCols)
+    staff2["staff_expertise_description"]="Agronomy and taxonomi"
+    staff2["staff_expertise_domain"]=["Ecology of mangrove", "ecology of small lakes"]
+    staff2["staff_expertise_geography"]="North Pacific coast"
+    staff2["staff_name"]="John Mondale"
+    staff2["staff_full_time_equivalent_pc"]=50
+    si2={}
+    si2["value"]="0000-0002-0626-540X"
+    si2["system"]="ORCID"
+    staff2["staff_identifiers"]=si2
+
+    natural_history_collection["collection_staff"]=[staff1, staff2]
+    returned_2["natural_history_collection"]=natural_history_collection
+
+    
     print("CREATE COLL")
     global INDEX_NAME
     print("created "+returned_2["full_path"])
